80f86868ee1cc6fd780921cbe252c680999c4ed2
Klihsman <klihsman2.0@gmail.com>
2021-06-01 17:48:29 -0300

    Iniciando estudos e criando componente App para controle

---
+++ ReactNative/src/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View} from 'react-native';
+
+import First from './components/first';
+
+const App = function () {
+  return (
+    <View>
+      <First />
+    </View>);
+};
+export default App;
